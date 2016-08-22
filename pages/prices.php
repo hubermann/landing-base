726291842658054e06a833921f0d9941e8d31938
hubermann <hubermann@gmail.com>
2016-08-21 23:44:23 -0300

    includes

---
+++ pages/prices.php
@@ -0,0 +1,74 @@
+
+<style type="text/css">
+.price-item{background-color: gray; padding: .8em 0;}
+.price-item .price {background-color: red; color: #fff}
+.price-item .price-heading{ text-align: center; color: #fff; padding:.8em; }
+.price-item .price-title h3{color: red; text-align: center;}
+.price-item .price{text-align: center; font-weight: 300; padding: .8em;}
+.price-item .price-details{ list-style: none; margin: 1em 0; }
+.price-details li{text-align:center; font-weight: 300; margin: .8em 0;}
+#prices-heading{text-align: center; padding:.8em;}
+
+</style>
+<section id="prices">
+	<div class="row" id="prices-heading">
+		<h2>Precios de productos o servicios</h2>
+		<p>Este modulo sirve para mostrar diferentes variantes de precios para productos o servicios.</p>
+	</div>
+	
+	<!-- item 1 -->
+	<div class="col-md-4 ">
+		<div class="price-item">
+			<div class="price-title">
+				<h3>Nombre del prooducto o servicio.</h3>
+			</div>
+			<div class="price text3">
+				$99.50
+			</div>
+			<ul class="price-details">
+				<li>Caracteristica uno</li>
+				<li>Ventajas de uso</li>
+				<li>Otra ventaja</li>
+				<li>Caracterisitica dos</li>
+			</ul>
+		</div>
+	</div>
+	<!-- item 2 -->
+	<div class="col-md-4 ">
+		<div class="price-item">
+			<div class="price-title">
+				<h3>Nombre del prooducto o servicio.</h3>
+			</div>
+			<div class="price text3">
+				$99.50
+			</div>
+			<ul class="price-details">
+				<li>Caracteristica uno</li>
+				<li>Ventajas de uso</li>
+				<li>Otra ventaja</li>
+				<li>Caracterisitica dos</li>
+			</ul>
+		</div>
+	</div>
+	
+	<!-- item 3-->
+	<div class="col-md-4 ">
+		<div class="price-item">
+			<div class="price-title">
+				<h3>Nombre del prooducto o servicio.</h3>
+			</div>
+			<div class="price text3">
+				$99.50
+			</div>
+			<ul class="price-details">
+				<li>Caracteristica uno</li>
+				<li>Ventajas de uso</li>
+				<li>Otra ventaja</li>
+				<li>Caracterisitica dos</li>
+			</ul>
+		</div>
+	</div>
+
+
+
+</section>
